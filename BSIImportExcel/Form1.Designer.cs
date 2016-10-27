401c605a33c7a0383e782b20fd98083d0012757c
jbalessandro <jb.alessandro@gmail.com>
2016-10-27 00:14:33 -0200

    Add project files.

---
+++ BSIImportExcel/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BSIImportExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.txtOrigem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tmImport = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnImportar.Location = new System.Drawing.Point(446, 255);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(75, 23);
+            this.btnImportar.TabIndex = 0;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // txtOrigem
+            // 
+            this.txtOrigem.Location = new System.Drawing.Point(33, 88);
+            this.txtOrigem.Name = "txtOrigem";
+            this.txtOrigem.Size = new System.Drawing.Size(403, 20);
+            this.txtOrigem.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Diretório origem";
+            // 
+            // tmImport
+            // 
+            this.tmImport.Enabled = true;
+            this.tmImport.Interval = 120000;
+            this.tmImport.Tick += new System.EventHandler(this.tmImport_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 330);
+            this.Controls.Add(this.txtOrigem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImportar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.TextBox txtOrigem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tmImport;
+    }
+}
+
---
+++ BSIImportExcel/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BSIImportExcel
+{
+    public partial class Form1 : Form
+    {
+        int _fstRow = 2;
+        int _colAgencia = 1;
+        int _colConta = 2;
+        string _connString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=local";
+        string _sqlString;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // TODO: remover aqui
+            txtOrigem.Text = @"C:\Users\ale\Desktop\BSI\ImportacaoExcel";
+            _sqlString = getCmdText();
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            importarArquivo();
+        }
+
+        private void importarArquivo()
+        {
+            foreach (var item in getArquivos())
+            {
+                Excel.Application appExcel = null;
+                Excel.Workbook workbook = null;
+                Excel.Worksheet workSheet = null;
+                Excel.Range range;
+
+                try
+                {
+                    appExcel = new Excel.Application();
+                    workbook = appExcel.Workbooks.Open(item, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                    workSheet = (Excel.Worksheet)workbook.Worksheets[1];
+                    range = workSheet.UsedRange;
+
+                    for (int i = _fstRow; i <= range.Rows.Count; i++)
+                    {
+                        if ((!string.IsNullOrEmpty((range.Cells[i, _colAgencia] as Excel.Range).Value.ToString())) &&
+                            !string.IsNullOrEmpty((range.Cells[i,_colConta] as Excel.Range).Value.ToString()))
+                        {
+                            // TODO: insert into access table
+                            Registro registro = new Registro();
+                            registro.Agencia = (range.Cells[i, _colAgencia] as Excel.Range).Value.ToString();
+                            registro.Conta = ((Excel.Range)range.Cells[i, _colConta]).Value.ToString();
+                            updateDataBase(registro);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    // TODO: log
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    workbook.Close(true, null, null);
+                    appExcel.Quit();
+
+                    releaseObject(workSheet);
+                    releaseObject(workbook);
+                    releaseObject(appExcel);
+
+                    try
+                    {
+                        moverArquivoProcessado(item);
+                    }
+                    catch (Exception ep)
+                    {
+                        // TODO: log falha em mover arquivo processado
+                        MessageBox.Show("Falha em mover arquivo processado: " + ep.Message);
+                    }
+                }
+            }
+        }
+
+        private void moverArquivoProcessado(string arquivo)
+        {
+            FileInfo file = new FileInfo(arquivo);
+            string processado = Path.Combine(txtOrigem.Text, "Processados", file.Name);
+
+            if (File.Exists(processado))
+            {
+                File.Delete(processado);
+            }
+            file.MoveTo(processado);
+        }
+
+        private List<string> getArquivos()
+        {
+            if (string.IsNullOrEmpty(txtOrigem.Text))
+            {
+                throw new ArgumentException("Diretório origem inválido");
+            }
+
+            if (!Directory.Exists(txtOrigem.Text))
+            {
+                throw new DirectoryNotFoundException("Diretório origem inexistente");
+            }
+
+            return Directory.GetFiles(txtOrigem.Text.Trim())
+                .Where(a => !a.Contains("~$") && (a.ToLower().EndsWith(".xls") || a.ToLower().EndsWith(".xlsx"))).ToList();
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                // TODO: log release object error
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void updateDataBase(Registro reg)
+        {
+            using (OleDbConnection conn = new OleDbConnection(_connString))
+            {
+                conn.Open();
+                using(OleDbCommand cmd = new OleDbCommand(_sqlString, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Agencia", reg.Agencia);
+                    cmd.Parameters.AddWithValue("@Conta", reg.Conta);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private string getCmdText()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("INSERT INTO Tabela (Agencia, Conta)")
+                .Append(" VALUES (@Agencia,@Conta)");
+
+            return sb.ToString();
+        }
+
+        private void tmImport_Tick(object sender, EventArgs e)
+        {
+            importarArquivo();
+        }
+    }
+
+    public class Registro
+    {
+        public string Agencia { get; set; }
+        public string Conta { get; set; }
+    }
+}
